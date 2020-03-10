ae9d2c3c46fc02dcbbc00ea9311caebf0e938842
huachuan <hc.5@qq.com>
2020-03-10 20:21:46 +0800

    1、数据更新。

--- resource/mood/keyword.js
+++ resource/mood/keyword.js
@@ -1,7 +1,7 @@
 const data = {
   "res": [
     {keyword: "控制", time: "2020-03-01"},
-    {keyword: "控制", time: "2020-03-02"},
+    {keyword: "花开", time: "2020-03-02"},
     {keyword: "世界", time: "2020-03-03"},
     {keyword: "中二", time: "2020-03-04"},
     {keyword: "失望", time: "2020-03-04"},
@@ -11,18 +11,27 @@ const data = {
     {keyword: "思念", time: "2020-03-08"},
     {keyword: "认真", time: "2020-03-09"},
     {keyword: "往前", time: "2020-03-10"},
-    {keyword: "控制", time: "2020-03-11"},
-    {keyword: "控制", time: "2020-03-12"},
-    {keyword: "控制", time: "2020-03-13"},
-    {keyword: "控制", time: "2020-03-14"},
-    {keyword: "控制", time: "2020-03-15"},
-    {keyword: "控制", time: "2020-03-16"},
-    {keyword: "控制", time: "2020-03-17"},
-    {keyword: "控制", time: "2020-03-18"},
-    {keyword: "控制", time: "2020-03-19"},
-    {keyword: "控制", time: "2020-03-20"},
-    {keyword: "控制", time: "2020-03-21"},
-    {keyword: "控制", time: "2020-03-22"}
+    {keyword: "故事", time: "2020-03-11"},
+    {keyword: "表情", time: "2020-03-12"},
+    {keyword: "长大", time: "2020-03-13"},
+    {keyword: "旅行", time: "2020-03-14"},
+    {keyword: "悲伤", time: "2020-03-15"},
+    {keyword: "怀念", time: "2020-03-16"},
+    {keyword: "开心", time: "2020-03-17"},
+    {keyword: "开心", time: "2020-03-18"},
+    {keyword: "开心", time: "2020-03-19"},
+    {keyword: "开心", time: "2020-03-20"},
+    {keyword: "开心", time: "2020-03-21"},
+    {keyword: "开心", time: "2020-03-22"},
+    {keyword: "开心", time: "2020-03-23"},
+    {keyword: "开心", time: "2020-03-24"},
+    {keyword: "开心", time: "2020-03-25"},
+    {keyword: "开心", time: "2020-03-26"},
+    {keyword: "开心", time: "2020-03-27"},
+    {keyword: "开心", time: "2020-03-28"},
+    {keyword: "开心", time: "2020-03-29"},
+    {keyword: "开心", time: "2020-03-30"},
+    {keyword: "开心", time: "2020-03-31"}
   ]
 }
 
