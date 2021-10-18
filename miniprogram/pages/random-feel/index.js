4a4ad31400395278a265112c3ce7696c6c0247b5
achuan <hc.5@qq.com>
2021-10-18 17:33:18 +0800

    随机心情页面开发；

--- miniprogram/pages/random-feel/index.js
+++ miniprogram/pages/random-feel/index.js
@@ -20,85 +20,113 @@ Page({
    */
   data: {
     state: '开始',
-    show: false,
-    bigtitlehide: false
+    randomVisible: false, // 
+    bigtitlehide: false,
   },
 
   // 开始随机
   async refreshRandom() {
-    // wx.showLoading({
-    //   title: '正在搜寻菜谱',
-    // })
+    wx.showLoading({
+      title: '正在搜寻中...',
+    })
 
     // 获取所有key类型
     let _self = this
     let keyTypeList = []
+    let tagTypeList = []
     const db = wx.cloud.database()
 
     await db.collection('key-type').get().then(res => {
-      console.log('ACHUAN : db.collection : res', res)
       // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
       let resT = res.data.filter(item => item.isVisible)
       keyTypeList = resT
-      // _self.setData({
-      //   keyTypeList: resT
-      // })
     }).catch(err => {
       console.error('ACHUAN : db.collection : err', err)
     })
-    console.log('ACHUAN : awaitdb.collection : keyTypeList', keyTypeList)
 
-    var randoms = keyTypeList
-    for (var i = 0; i < randoms.length; i++) {
-      var fontsize = 20 + Math.floor(Math.random() * randoms.length)
-      var x = Math.floor(Math.random() * 70)
+    await wx.cloud.callFunction({
+      name: 'getTagType'
+    }).then(res => {
+      if (res) {
+        tagTypeList = res.result.data
+      }
+    }).catch((err) => {
+      console.error('ACHUAN : db.collection : err', err)
+    })
+
+    wx.hideLoading({})
+
+    // 随机心情
+    function shuffle(arr) {
+      var len = arr.length;
+      for (var i = 0; i < len - 1; i++) {
+        var index = parseInt(Math.random() * (len - i));
+        var temp = arr[index];
+        arr[index] = arr[len - i - 1];
+        arr[len - i - 1] = temp;
+      }
+      return arr;
+    }
+
+    var keyTypeListT = shuffle(JSON.parse(JSON.stringify(keyTypeList.concat(tagTypeList)))); //结果不唯一
+    // 循环出随机的坐标位置和字体大小
+    for (var i = 0; i < keyTypeListT.length; i++) {
+      console.log(Math.floor(Math.random() * 70));
+      var fontsize = 30 + Math.floor(Math.random() * 40)
+      var x = Math.floor(Math.random() * 80)
       var y = Math.floor(Math.random() * 70)
-      randoms[i].fontsize = fontsize
-      randoms[i].x = x
-      randoms[i].y = y
+      keyTypeListT[i].fontsize = fontsize
+      keyTypeListT[i].x = x
+      keyTypeListT[i].y = y
+      keyTypeListT[i].show = false
     }
 
     _self.setData({
-      randoms: randoms
+      randoms: keyTypeListT
     })
+
+
     animate = setInterval(function () {
+      // 计时器让randoms显示出来
       _self.setData({
         ['randoms[' + nowAnimate + '].show']: true
       })
       nowAnimate++
+      console.log('ACHUAN : nowAnimate', nowAnimate)
+      // 长度相等的时候
       if (nowAnimate == _self.data.randoms.length) {
-        var random_index = Math.floor(Math.random() * _self.data.randoms.length)
-        _self.setData({
-          state: '换一个',
-          show: true,
-          random: _self.data.randoms[random_index]
-        })
+        console.log('ACHUAN : animate', animate)
         if (animate) {
-          // wx.hideLoading({})
           clearInterval(animate)
+          _self.setData({
+            state: '停止',
+            bigtitlehide: true,
+            randomVisible: false,
+            randoms: []
+          })
+          nowAnimate = 0
+          _self.refreshRandom()
         }
-        nowAnimate = 0
       }
-    }, 500)
+    }, 900)
   },
 
-  // 开始随机
+  // 开始随机按钮
   onButton() {
     var that = this
-    if (this.data.state == '开始' || this.data.state == '换一个') {
+    if (this.data.state == '开始') {
       this.setData({
         state: '停止',
         bigtitlehide: true,
-        show: false,
+        randomVisible: false,
         randoms: []
       })
       this.refreshRandom()
-    }
-    else if (this.data.state == '停止') {
+    } else if (this.data.state == '停止') {
       var random_index = Math.floor(Math.random() * this.data.randoms.length)
       this.setData({
-        state: '换一个',
-        show: true,
+        state: '开始',
+        randomVisible: true,
         random: this.data.randoms[random_index]
       })
       if (animate) {
@@ -106,61 +134,22 @@ Page({
       }
       nowAnimate = 0
     }
-
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 点击心情
+  onFeel(event) {
+    console.log('ACHUAN : onFeel : event', event)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onHide() {
+    this.setData({
+      state: '开始',
+      bigtitlehide: false,
+      randomVisible: false,
+      randoms: []
+    })
+    clearInterval(animate)
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
